274bd21c0376ea333d5075270cd60bd4e76b2f92
shreya-schandup <107084633+shreya-schandup@users.noreply.github.com>
2022-09-06 20:39:18 -0500

    Add files via upload

---
+++ schandup_lab02.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include <time.h> 
+#include <stdlib.h> 
+
+
+int main(){
+    srand48((unsigned int)time(NULL));  
+    int N, i, currLoc;
+    double arr[100],temp;
+
+    printf("Enter the value how many random numbers are to be generated:\n");
+    scanf("%d",&N);
+    for(i=0;i<N;i++){
+        arr[i] = drand48(); 
+        printf("%lf\n",arr[i]);
+    }
+    printf("\n");
+
+    printf("The Unsorted Array Elements are:\n");
+    for(i = 0;i<N;i++){
+        printf("%lf\n",arr[i]);
+    }
+    printf("\n");
+
+	for (int i=1; i<N; i++){
+	    currLoc = i;
+	    while (currLoc > 0 && arr[currLoc-1] > arr[currLoc]){
+		temp = arr[currLoc];
+		arr[currLoc] = arr[currLoc-1];
+		arr[currLoc-1] = temp;
+		currLoc--;
+	    }
+	}
+    printf("\n");
+    printf("The Sorted Array is:\n");
+    for(i=0;i<N;i++){
+        printf("%lf\n",arr[i]);
+    }
+		
+}
